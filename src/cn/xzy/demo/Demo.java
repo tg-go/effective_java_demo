6fff1db7dbb045f90c158f75f9c2b71b14e1c3f9
luobo <luobo@baijia.com>
2023-01-05 18:31:56 +0800

    add demo

---
+++ src/cn/xzy/demo/Demo.java
@@ -0,0 +1,11 @@
+package cn.xzy.demo;
+
+/**
+ * @Date 2023-01-05
+ **/
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Effective Java");
+    }
+}
